a380aef02d46cac44816ff2fc9d6e38137a0b8b3
nick@nyk.ch <nick@nyk.ch>
2018-09-25 12:01:32 +0200

    added config file

--- ikea_tradfri_log_analyze.R
+++ ikea_tradfri_log_analyze.R
@@ -1,7 +1,9 @@
 library(data.table)
+library(rjson)
+cfg <- fromJSON(file="/opt/ikea_tradfri_config.txt")
 
 # Load and prepare data
-tradfri_log <- fread("tradfri.txt") # Load logfile created by ikea_tradfi_alive.py
+tradfri_log <- fread(cfg[["logfile"]]) # Load logfile created by ikea_tradfi_alive.py
 colnames(tradfri_log) <- c("timestamp", "bulb", "alive", "power", "brightness", "warmth") # Add column headers
 tradfri_log$ptime <- as.POSIXct(tradfri_log$timestamp, origin="1970-01-01") # Convert unix timestamp
 tradfri_log$time <- strftime(tradfri_log$ptime, "%H:%M") # Create hour:minute variable
@@ -20,4 +22,4 @@ sprintf("%d of %d (%2.1f%%) missing", missing_minutes, length(minutes), 100*miss
 tradfri_day[alive == 0 & power == 1, "power"] <- 0 # Not alive -> no power
 tradfri_day <- tradfri_day[,c("bulb", "time", "power", "brightness", "warmth")] # Select required columns
 print(table(tradfri_day$bulb, tradfri_day$power)) # Tabulate number of ON minutes per bulb and day
-fwrite(tradfri_day, file="tradfri_day.tsv", sep="\t", col.names = FALSE) # Save as TSV text
+fwrite(tradfri_day, file=cfg[["dayfile"]], sep="\t", col.names = FALSE) # Save as TSV text
